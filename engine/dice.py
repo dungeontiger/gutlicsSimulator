16962fdd7bc426afb401facaf1345276ebbd1ea6
Stephen D. Gibson <Stephen.d.gibson@gmail.com>
2018-06-19 07:55:02 -0400

    Added more rolling methods

--- engine/dice.py
+++ engine/dice.py
@@ -1,4 +1,4 @@
-import random
+import random, re
 """ Perform all kinds of dice rols """
 class Dice:
 
@@ -8,3 +8,37 @@ class Dice:
     for _ in range(1, numberOfDice + 1):
       sum = sum + random.randint(1, sides)
     return sum + modifier
+
+  """Short cut to roll a d20 """
+  def d20(self):
+    return self.roll(1,20,0)
+
+  """Roll d20 with advantage, roll two and return the highest """
+  def d20Advantage(self):
+    roll1 = self.roll(1,20,0)
+    roll2 = self.roll(1,20,0)
+    if roll1 > roll2:
+      return roll1
+    return roll2
+
+  """Roll d20 with disadvantage, roll two and return the lowest """
+  def d20Disadvantage(self):
+    roll1 = self.roll(1,20,0)
+    roll2 = self.roll(1,20,0)
+    if roll1 < roll2:
+      return roll1
+    return roll2
+
+  """ split a string into the correct integer parts and return the roll e.g. 3d6+2 """
+  def rollString(self, roll):
+    # first remove spaces from string to make regex easy
+    # TODO: Throw exception for bad string?
+    s = roll.replace(' ', '')
+    m = re.match(r"(\d*)[dD](\d*)(.*)", s).groups()
+    dice = int(m[0])
+    sides = int(m[1])
+    mod = 0
+    if (m[2] != ''):
+      mod = int(m[2])
+    return self.roll(dice, sides, mod)
+    
--- engine/test/test_dice.py
+++ engine/test/test_dice.py
@@ -1,10 +1,69 @@
 import unittest
 from engine.dice import Dice
+
 class TestDice (unittest.TestCase):
+  """Test a single D20 roll """
   def testSimpleRoll(self):
     roller = Dice()
     value = roller.roll(1, 20, 0)
     self.assertTrue(value <= 20 and value >= 1, 'Value was {}'.format(value))
+
+  """Test a roll of multiple dice """
+  def testMultipleDice(self):
+    roller = Dice()
+    value = roller.roll(3,6,0)
+    self.assertTrue(value <= 18 and value >= 3)
+
+  """Test with a modifier """
+  def testWithModifer(self):
+    roller = Dice()
+    value = roller.roll(1, 10, 5)
+    self.assertTrue(value <= 15 and value >= 6)
+
+  """Roll 100 times and ensure that the min and max are rolled, its possible to fail but unlikely """
+  def testRoll100Times(self):
+    roller = Dice()
+    found1 = False
+    found15 = False
+    for _ in range (0, 100):
+      value = roller.roll(2,8,-1)
+      self.assertTrue(value <= 15 and value >= 1)
+      if value == 1:
+        found1 = True
+      if value == 15:
+        found15 = True
+    self.assertTrue(found1, 'Did not roll a 1')
+    self.assertTrue(found15, 'Did not roll a 15')
+
+  """Test d20 roll"""
+  def testD20(self):
+    roller = Dice()
+    value = roller.d20()
+    self.assertTrue(value >= 1 and value <= 20)
+
+  """Test d20 advantage roll"""
+  def testD20Advantage(self):
+    roller = Dice()
+    value = roller.d20Advantage()
+    self.assertTrue(value >= 1 and value <= 20)
+
+  """Test d20 disadvantage roll"""
+  def testD20Disadvantage(self):
+    roller = Dice()
+    value = roller.d20Disadvantage()
+    self.assertTrue(value >= 1 and value <= 20)
+
+  """Test rolling by string"""
+  def testRollByString(self):
+    roller = Dice()
+    value = roller.rollString('3d6')
+    self.assertTrue(value >= 3 and value <= 18)
+
+  """Test rolling by string with a modifer """
+  def testRollByStringMod(self):
+    roller = Dice()
+    value = roller.rollString('2d8-1')
+    self.assertTrue(value >= 1 and value <= 15)
 
 if __name__ == '__main__':
     unittest.main() 
